b41243421189a3160a3835052cf161ca1f34232c
marty12 <>
2017-10-23 22:58:46 +0200

    Removed last.fm php library (it is not being used)

--- src/ScrobblerBundle/Controller/DefaultController.php
+++ src/ScrobblerBundle/Controller/DefaultController.php
@@ -6,7 +6,6 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use SpotifyWebAPI;
 use Symfony\Component\HttpFoundation\Request;
-use LastFmApi;
 
 /**
  * Class DefaultController
