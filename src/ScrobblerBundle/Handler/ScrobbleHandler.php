14d604a6b8c868e45d4df18e22c08919ac2f3342
marty12 <>
2017-10-28 23:23:41 +0200

    -Moved last.fm logic to the new last.fm api class -Some refactoring to the scrobbling logic -Added phpdoc data

--- src/ScrobblerBundle/Handler/ScrobbleHandler.php
+++ src/ScrobblerBundle/Handler/ScrobbleHandler.php
@@ -2,273 +2,114 @@
 
 namespace ScrobblerBundle\Handler;
 
-use GuzzleHttp\Client;
+use ScrobblerBundle\Api\LastFm;
 
 // todo decouple scrobble handler and last.fm implementation
-// todo 2: much duplication here; merge calls (mostly api related stuff, such as signature)
 
 class ScrobbleHandler
 {
     const PROGRESS_FOR_SCROBBLE = 0.75;
 
-    /** @var Client */
-    private $httpClient;
-
-    /** @var string */
-    private $lastFmApiKey;
-
-    /** @var string */
-    private $lastFmSessionId;
-
-    /** @var string */
-    private $lastFmApiSecret;
-
-    /** @var string */
-    private $lastFmApiUrl;
-
-    /** @var string */
-    private $lastFmUsername;
-
-    public function __construct($lastFmApiKey, $lastFmSessionId, $lastFmApiSecret, $lastFmApiUrl, $lastFmUsername)
-    {
-        $this->setLastFmApiKey($lastFmApiKey)
-            ->setLastFmSessionId($lastFmSessionId)
-            ->setLastFmApiSecret($lastFmApiSecret)
-            ->setLastFmApiUrl($lastFmApiUrl)
-            ->setLastFmUsername($lastFmUsername);
-
-        $this->setHttpClient(new Client());
-    }
+    /** @var LastFm */
+    private $lastFmApi;
 
     /**
-     * @return Client
+     * ScrobbleHandler constructor.
+     * @param LastFm $lastFmApi
      */
-    public function getHttpClient(): Client
+    public function __construct(LastFm $lastFmApi)
     {
-        return $this->httpClient;
+        $this->lastFmApi = $lastFmApi;
     }
 
     /**
-     * @param Client $httpClient
+     * @return LastFm
+     */
+    public function getLastFmApi(): LastFm
+    {
+        return $this->lastFmApi;
+    }
+
+    /**
+     * @param LastFm $lastFmApi
      * @return ScrobbleHandler
      */
-    public function setHttpClient(Client $httpClient): ScrobbleHandler
+    public function setLastFmApi(LastFm $lastFmApi): ScrobbleHandler
     {
-        $this->httpClient = $httpClient;
-        return $this;
-    }
-
-
-    /**
-     * @return string
-     */
-    protected function getLastFmApiKey(): string
-    {
-        return $this->lastFmApiKey;
-    }
-
-    /**
-     * @param string $lastFmApiKey
-     * @return ScrobbleHandler
-     */
-    protected function setLastFmApiKey(string $lastFmApiKey): ScrobbleHandler
-    {
-        $this->lastFmApiKey = $lastFmApiKey;
+        $this->lastFmApi = $lastFmApi;
         return $this;
     }
 
     /**
-     * @return string
-     */
-    protected function getLastFmSessionId(): string
-    {
-        return $this->lastFmSessionId;
-    }
-
-    /**
-     * @param string $lastFmSessionId
+     * @param array $trackData
      * @return ScrobbleHandler
      */
-    protected function setLastFmSessionId(string $lastFmSessionId): ScrobbleHandler
+    public function updateCurrentlyPlaying(array $trackData): ScrobbleHandler
     {
-        $this->lastFmSessionId = $lastFmSessionId;
-        return $this;
-    }
-
-    /**
-     * @return string
-     */
-    protected function getLastFmApiSecret(): string
-    {
-        return $this->lastFmApiSecret;
-    }
-
-    /**
-     * @param string $lastFmApiSecret
-     * @return ScrobbleHandler
-     */
-    protected function setLastFmApiSecret(string $lastFmApiSecret): ScrobbleHandler
-    {
-        $this->lastFmApiSecret = $lastFmApiSecret;
-        return $this;
-    }
-
-    /**
-     * @return string
-     */
-    protected function getLastFmApiUrl(): string
-    {
-        return $this->lastFmApiUrl;
-    }
-
-    /**
-     * @param string $lastFmApiUrl
-     * @return ScrobbleHandler
-     */
-    protected function setLastFmApiUrl(string $lastFmApiUrl): ScrobbleHandler
-    {
-        $this->lastFmApiUrl = $lastFmApiUrl;
-        return $this;
-    }
-
-    /**
-     * @return string
-     */
-    protected function getLastFmUsername(): string
-    {
-        return $this->lastFmUsername;
-    }
-
-    /**
-     * @param string $lastFmUsername
-     * @return ScrobbleHandler
-     */
-    protected function setLastFmUsername(string $lastFmUsername): ScrobbleHandler
-    {
-        $this->lastFmUsername = $lastFmUsername;
-        return $this;
-    }
-
-    public function updateCurrentlyPlaying(array $trackData)
-    {
-        $currentlyPlayingLastFmData = $this->getCurrentlyPlayingInLastFm();
-        if ($currentlyPlayingLastFmData['artist'] == $trackData['track_artist']
-            && $currentlyPlayingLastFmData['title'] == $trackData['track_title']) {
-            return;
+        if (! $this->currentlyPlayingShouldBeUpdated($trackData)) {
+            return $this;
         }
 
-        $signature = 'album' . $trackData['track_album'];
-        $signature .= 'api_key' . $this->getLastFmApiKey();
-        $signature .= 'artist' . $trackData['track_artist'];
-        $signature .= 'methodtrack.updateNowPlaying';
-        $signature .= 'sk' . $this->getLastFmSessionId();
-        $signature .= 'track' . $trackData['track_title'];
-        $signature .= $this->getLastFmApiSecret();
+        $this->getLastFmApi()
+            ->updateCurrentlyPlaying($trackData['track_artist'], $trackData['track_title'], $trackData['track_album']);
 
-        $signature = md5($signature);
-
-        $queryParameters = array(
-            'artist' => $trackData['track_artist'],
-            'track'  => $trackData['track_title'],
-            'album' => $trackData['track_album'],
-            'api_key' => $this->getLastFmApiKey(),
-            'api_sig' => $signature,
-            'sk'    => $this->getLastFmSessionId(),
-            'method' => 'track.updateNowPlaying',
-            'format' => 'json'
-        );
-
-        $client = new Client();
-        $response = $client->post($this->getLastFmApiUrl(), ['form_params' => $queryParameters]);
-
-        // debug stuff
-        $responseArray = json_decode($response->getBody()->getContents(), true);
-        $artist = $responseArray['nowplaying']['artist']['#text'];
-        $album = $responseArray['nowplaying']['album']['#text'];
-        $title = $responseArray['nowplaying']['track']['#text'];
-
-        echo sprintf("now playing %s - %s (%s) \n", $artist, $title, $album);
+        return $this;
     }
 
-    public function trackShouldBeScrobbled($trackData)
+    /**
+     * @param $trackData
+     * @return ScrobbleHandler
+     */
+    public function handleTrackScrobbling($trackData): ScrobbleHandler
+    {
+        if ($this->trackShouldBeScrobbled($trackData)) {
+            $this->getLastFmApi()
+                ->scrobbleTrack($trackData['track_artist'], $trackData['track_title'], $trackData['track_album'], time());
+        }
+
+        return $this;
+    }
+
+    /**
+     * @param $trackData
+     * @return bool
+     */
+    protected function currentlyPlayingShouldBeUpdated($trackData): bool
+    {
+        $trackData = $this->getSanitizedDataForComparison($trackData);
+        $currentlyPlayingLastFmData = $this->getSanitizedDataForComparison($this->getLastFmApi()->getCurrentlyPlaying());
+
+        return ! ($currentlyPlayingLastFmData['artist'] == $trackData['track_artist']
+            && $currentlyPlayingLastFmData['title'] == $trackData['track_title']);
+    }
+
+    /**
+     * @param $trackData
+     * @return bool
+     */
+    protected function trackShouldBeScrobbled($trackData): bool
     {
         if (($trackData['current_track_progress'] / $trackData['track_total_time']) <= self::PROGRESS_FOR_SCROBBLE) {
             return false;
         }
 
-        $lastScrobbledTrackData = $this->getLastScrobbledTrack();
-        return ($lastScrobbledTrackData['artist'] != $trackData['track_artist']
-            && $lastScrobbledTrackData['title'] != $trackData['track_title']);
+        $lastScrobbledTrackData = $this->getSanitizedDataForComparison($this->getLastFmApi()->getLastScrobbledTrack());
+        $trackData = $this->getSanitizedDataForComparison($trackData);
+
+        return ! ($lastScrobbledTrackData['artist'] == $trackData['track_artist']
+            && $lastScrobbledTrackData['title'] == $trackData['track_title']);
     }
 
-    public function scrobbleTrack($trackData)
+    /**
+     * @param $data
+     * @return array
+     */
+    protected function getSanitizedDataForComparison(array $data): array
     {
-        $queryParameters = array(
-            'artist' => $trackData['track_artist'],
-            'track'  => $trackData['track_title'],
-            'album' => $trackData['track_album'],
-            'timestamp' => $trackData['timestamp'],
-            'api_key' => $this->getLastFmApiKey(),
-            'api_sig' => $this->getLastFmFunctionSignature($trackData),
-            'sk'    => $this->getLastFmSessionId(),
-            'method' => 'track.scrobble',
-            'format' => 'json'
-        );
+        foreach ($data as &$value) {
+            $value = strtolower(trim($value));
+        }
 
-        $response = $this->getHttpClient()
-            ->post($this->getLastFmApiUrl(), ['form_params' => $queryParameters]);
-
-        // debug stuff
-        $responseArray = json_decode($response->getBody()->getContents(), true);
-        $artist = $responseArray['scrobbles']['scrobble']['artist']['#text'];
-        $album = $responseArray['scrobbles']['scrobble']['album']['#text'];
-        $title = $responseArray['scrobbles']['scrobble']['track']['#text'];
-
-        echo sprintf("scrobbled %s - %s (%s) \n", $artist, $title, $album);
-    }
-
-    protected function getLastFmFunctionSignature($trackData)
-    {
-        $signature = 'album' . $trackData['track_album'];
-        $signature .= 'api_key' . $this->getLastFmApiKey();
-        $signature .= 'artist' . $trackData['track_artist'];
-        $signature .= 'methodtrack.scrobble';
-        $signature .= 'sk' . $this->getLastFmSessionId();
-        $signature .= 'timestamp' . $trackData['timestamp'];
-        $signature .= 'track' . $trackData['track_title'];
-        $signature .= $this->getLastFmApiSecret();
-
-        return md5($signature);
-    }
-
-    protected function getLastScrobbledTrack()
-    {
-        return $this->getRecentlyScrobbledTrack(1);
-    }
-
-    protected function getCurrentlyPlayingInLastFm()
-    {
-        return $this->getRecentlyScrobbledTrack(0);
-    }
-
-    protected function getRecentlyScrobbledTrack($position)
-    {
-        $queryParameters = array(
-            'limit' => $position + 1,
-            'user' => $this->getLastFmUsername(),
-            'api_key' => $this->getLastFmApiKey(),
-            'method' => 'user.getrecenttracks',
-            'format' => 'json',
-            'nowplaying' => true // does not seem to have any effect, so set anyway
-        );
-
-        $response = $this->getHttpClient()
-            ->get($this->getLastFmApiUrl(), ['query' => $queryParameters]);
-
-        $responseArray = json_decode($response->getBody()->getContents(), true);
-
-        $trackData = $responseArray['recenttracks']['track'][$position];
-
-        return ['artist' => $trackData['artist']['#text'], 'title' => $trackData['name']];
+        return $data;
     }
 }
